1deeb09ac631a2f5c3c69829fb7e8147ade640ce
Romain Libaert <ro.lib@pm.me>
2025-05-06 16:07:32 +0200

    feat: test pool

---
+++ pool_test.go
@@ -0,0 +1,33 @@
+package sync_test
+
+import (
+	"testing"
+
+	"github.com/oaiiae/sync-generic"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPool(t *testing.T) {
+	t.Run("returns zero value", func(t *testing.T) {
+		p := new(sync.Pool[[]byte])
+		require.Equal(t, []byte(nil), p.Get())
+		p = p.New(nil)
+		require.Equal(t, []byte(nil), p.Get())
+	})
+
+	t.Run("returns new value", func(t *testing.T) {
+		p := new(sync.Pool[[]byte]).New(func() []byte { return make([]byte, 8) })
+		x := p.Get()
+		require.NotEqual(t, []byte(nil), x)
+		require.Len(t, x, 8)
+	})
+
+	t.Run("returns existing value", func(t *testing.T) {
+		p := new(sync.Pool[[]byte])
+		require.Equal(t, []byte(nil), p.Get())
+		x := make([]byte, 8)
+		p.Put(x)
+		require.Equal(t, x, p.Get())
+		require.Equal(t, []byte(nil), p.Get())
+	})
+}

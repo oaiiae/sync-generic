732a7de5a73bd6b8e02ade5436d2359f68c84b27
Romain Libaert <git@rlibaert.dev>
2025-05-06 21:45:34 +0200

    fix(pool): cannot test Put
    We cannot assume any relation between Put & Get operation so there is no
    way to write a test checking the Put operation that *always* works.

--- pool_test.go
+++ pool_test.go
@@ -1,9 +1,7 @@
 package sync_test
 
 import (
-	"slices"
 	"testing"
-	"time"
 
 	"github.com/oaiiae/sync-generic"
 	"github.com/stretchr/testify/require"
@@ -22,14 +20,5 @@ func TestPool(t *testing.T) {
 		x := p.Get()
 		require.NotEqual(t, []byte(nil), x)
 		require.Len(t, x, 8)
-	})
-
-	t.Run("returns existing value", func(t *testing.T) {
-		p := new(sync.Pool[[]byte])
-		require.Equal(t, []byte(nil), p.Get())
-		x := make([]byte, 8)
-		p.Put(x)
-		require.Eventually(t, func() bool { return slices.Compare(p.Get(), x) == 0 }, 10*time.Second, time.Second/10)
-		require.Equal(t, []byte(nil), p.Get())
 	})
 }
